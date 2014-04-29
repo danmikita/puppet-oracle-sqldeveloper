848f0578c65fe0216d310f200d448eef21ece82d
Dan Mikita <danmikita@gmail.com>
2014-04-29 13:36:27 -0400

    Update oracle_sqldeveloper_spec.rb

--- spec/classes/oracle_sqldeveloper_spec.rb
+++ spec/classes/oracle_sqldeveloper_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe 'oracle_sqldeveloper' do
   it do
     should contain_package('SQLDeveloper').with({
-      :provider => 'pkgdmg',
-      :source   => 'https://dl.dropboxusercontent.com/s/u39vftqojzumob2/sqldeveloper-4.0.1.14.48-macosx.dmg'
+      :provider => 'compressed_app',
+      :source   => 'https://dl.dropboxusercontent.com/s/89rghfpkjn8m5vv/sqldeveloper-4.0.1.14.48-macosx.gz'
     })
   end
 end
